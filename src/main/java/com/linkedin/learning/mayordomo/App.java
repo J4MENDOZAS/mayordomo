9958186a12830274fa0da2dafd3c38c7665e0c41
Joseph Mendoza <jmendozas@quality-services.mx>
2021-12-10 13:50:37 -0600

    Se modifica Mensaje

--- src/main/java/com/linkedin/learning/mayordomo/App.java
+++ src/main/java/com/linkedin/learning/mayordomo/App.java
@@ -6,7 +6,7 @@ package com.linkedin.learning.mayordomo;
  */
 public class App {
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+	System.out.println("Proyecto Maven Compilado V1");
 	for (String s : args) {
 	    System.out.println(decora(s));
 	}
